422701c040a2893e2523b0e28f8c040a255be8f7
anayet71 <ariyananayet@gmail.com>
2025-01-24 00:47:03 +0600

    Conditional button added

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,11 +1,29 @@
+import { useState } from 'react';
 import './Country.css';
-const Country = ({country}) => {
+const Country = ({ country, handleVisitedCountry }) => {
     console.log(country)
-    const {name, flags} = country
+    const { name, flags, population, area, cca3 } = country
+
+    const [visited, setVisited] = useState(false) //Hold 
+
+    const handleVisited = () => { //Handle
+        setVisited(!visited)
+    }
+
+    console.log(handleVisitedCountry)
+
     return (
-        <div className='country'>
-            <h3>Name: {name?.common}</h3>
-            <div><img src={flags.png} alt="" /></div>
+        <div className={`country ${visited && 'visited'}`}>
+            <h3 style={{ color: visited ? 'green' : 'red' }} >Name: {name?.common}</h3>
+            <img src={flags.png} alt="" />
+            <p>Population: {population}</p>
+            <p>Area: {area} sqft</p>
+            <p><small>Code: {cca3}</small></p>
+            <button onClick={handleVisitedCountry}>Mark visited</button>
+             <br />  <br />
+            <button onClick={handleVisited}>{visited ? 'visited' : 'Going'}</button> {/* Triggered here */}
+          
+            {visited ? 'I have visited this country' : 'I want to visit'}
         </div>
     );
 };
